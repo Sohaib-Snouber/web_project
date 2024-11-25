f5c0fb2558ef3cc63238a8c677eab233a2e3b2c1
Sohaib Snouber <sohaibshehabsnouber@gmail.com>
2024-11-25 12:29:30 +0100

    making a test file for sending emails with AWS SDK
    but this method is only now working for verified
    emails from the AWS, but this restrict can be
    removed when we have a real domain(not local), by
    applying the domain on the AWS for unverified users.

---
+++ task-manager-backend/send_email.js
@@ -0,0 +1,28 @@
+require('dotenv').config({ path: '../.env' });
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
+
+// Create SES client using credentials from .env
+const client = new SESClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
+
+const params = {
+  Source: process.env.SES_SOURCE_EMAIL, // Get source email from .env
+  Destination: {
+    ToAddresses: "sohaibshehabsnouber@gmail.com", // destination email
+  },
+  Message: {
+    Subject: { Data: "Test Email from Amazon SES" },
+    Body: { Text: { Data: "Hello! This is a test email." } },
+  },
+};
+
+const command = new SendEmailCommand(params);
+
+client.send(command)
+  .then((data) => console.log("Email sent successfully:", data))
+  .catch((error) => console.error("Error sending email:", error));
